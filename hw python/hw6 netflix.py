b02a074ffafe2a6957e078e855a80778a59d56d1
slattchrome <112937058+slattchrome@users.noreply.github.com>
2023-03-14 01:18:24 +0300

    6th home assigment

--- hw python/hw6 netflix.py
+++ hw python/hw6 netflix.py
@@ -1,27 +1,21 @@
 import random
 
+
 COST = random.randint(50, 101)
 
 personalMoney = { "Alice":random.randint(0, 80),
         "Bob":random.randint(0, 80),
         "Charley":random.randint(0, 80) }
-
-
+#создаю список в котором будет генерироваться баланс ребят
 
 while (personalMoney["Alice"] + personalMoney["Bob"] + personalMoney["Charley"] < COST):
     personalMoney["Alice"]: random.randint(0, 80)
     personalMoney["Bob"]: random.randint(0, 80)
     personalMoney["Charley"]: random.randint(0, 80)
 
-max_value = max(personalMoney.values())
-finalPersonalMoney = {k: v for k, v in personalMoney.items() if v == max_value}
-print(finalPersonalMoney)
+    print(personalMoney)
+#создаю цикл для того, чтобы соблюдалось условие x+y+z > COST
+personalMoney.pop(min(personalMoney))
+#убираю наименьшее значение из списка, тем самым остаются двое с наибольшими значениями, которые будут скидываться на подписку
 
-if personalMoney["Bob"] < personalMoney["Alice"] < personalMoney["Charley"] or personalMoney["Charley"] < personalMoney["Alice"] < personalMoney["Bob"]:
-    finalPersonalMoney.update(personalMoney["Alice"])
-elif personalMoney["Alice"] < personalMoney["Bob"] < personalMoney["Charley"] or personalMoney["Charley"] < personalMoney["Bob"] < personalMoney["Alice"]:
-    finalPersonalMoney.update(personalMoney["Bob"])
-else:
-    finalPersonalMoney.update(personalMoney["Charley"])
-
-print(finalPersonalMoney)
+print("На подписку будут скидываться: " ''.join(map(str, personalMoney)))
