560a176ce41a3cd42fe2ec05e2ddde28b513595b
slattchrome <112937058+slattchrome@users.noreply.github.com>
2023-03-14 11:43:21 +0300

    updated 6th assignment

--- hw python/hw6 netflix.py
+++ hw python/hw6 netflix.py
@@ -13,9 +13,8 @@ while (personalMoney["Alice"] + personalMoney["Bob"] + personalMoney["Charley"] 
     personalMoney["Bob"]: random.randint(0, 80)
     personalMoney["Charley"]: random.randint(0, 80)
 
-    print(personalMoney)
 #создаю цикл для того, чтобы соблюдалось условие x+y+z > COST
 personalMoney.pop(min(personalMoney))
 #убираю наименьшее значение из списка, тем самым остаются двое с наибольшими значениями, которые будут скидываться на подписку
-
-print("На подписку будут скидываться: " ''.join(map(str, personalMoney)))
+print("На подписку будут скидываться: ")
+print( ''.join(personalMoney))
